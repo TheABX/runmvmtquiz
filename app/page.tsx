9cd66e0e050356c227218d521508dfb4c9685e69
deon kenzie <deonkenzie@deons-MacBook-Pro.local>
2025-12-06 17:01:20 +1100

    Update: Change homepage redirect to RUN MVMT quiz

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,6 @@
-'use client'
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  const router = useRouter()
-  
-  useEffect(() => {
-    router.push('/lifephoria-lp1')
-  }, [router])
-
-  return (
-    <div className="min-h-screen bg-background flex items-center justify-center">
-      <div className="text-center">
-        <p className="text-text-secondary">Redirecting to quiz...</p>
-      </div>
-    </div>
-  )
+  redirect('/runmvmt-quiz')
 }
 
